4dd106a6f18777cc9eab746bddca3cf87704426b
Sherri Mitchell <rsamitchell@att.net>
2015-09-30 21:13:27 -0400

    Add Users new and create actions and views. WIP.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = 'User created successfully.'
+			render :show
+		else
+			flash[:error] = 'Your entry was incorrect. Please try again.'
+		end
+		render
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :username, :password,
+                                   :about_me, :avatar, :twitter, :github, :linkedin)
+	end
+end
