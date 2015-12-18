541c65172aeee94ab021d2c7a5e81bbb937ff798
Sherri Mitchell <rsamitchell@att.net>
2015-12-17 22:43:51 -0500

    Adding changes to gemfile and gemfile.lock. Added Factory Girl gem. Adding changes to user.rb and user_spec.rb files. Updated rails_helper.rb and spec_helper files for Factory Girl gems.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,84 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe "User" do
-  	it "is invalid without a first_name"
-  	it "is invalid without a last_name"
-  	it "is invalid without an email"
-  	it "is invalid without a username"
-  	it "is invalid without a password"
-  	it "is invalid without an about_me description"
-  	it "is invalid without an avatar url"
-  	it "is invalid without a twitter url"
-  	it "is invalid without a github url"
-  	it "is invalid without a linkedin url"
+    it "it should make a user" do
+    user = User.new
+    expect(user).to be_a(User)
   end
 
-  it "returns a user's first_name as a string"
+  it "should be valid"
+  user = 
+  end
+
+    it "does not save a user record when the user does not provide an email address" do
+    user = User.new( …params not including the email address…)
+    expect(user.save).to return("error: an email address is required")
+  end
+
+    it "is invalid without a first_name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a last_name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).not_to include("can't be blank")
+  end
+
+    it "is invalid without an email" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a username" do
+    user = User.new(username: nil)
+    user.valid?
+    expect(user.errors[:username]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a password" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).not_to include("can't be blank")
+  end
+
+    it "is invalid without an about_me description" do
+    user = User.new(about_me: nil)
+    user.valid?
+    expect(user.errors[:about_me]).not_to include("can't be blank")
+  end
+
+    it "is invalid without an avatar url" do
+    user = User.new(avatar: nil)
+    user.valid?
+    expect(user.errors[:avatar]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a twitter url" do
+    user = User.new(twitter: nil)
+    user.valid?
+    expect(user.errors[:twitter]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a github url" do
+    user = User.new(github: nil)
+    user.valid?
+    expect(user.errors[:github]).not_to include("can't be blank")
+  end
+
+    it "is invalid without a linkedin url" do
+    user = User.new(linkedin: nil)
+    user.valid?
+    expect(user.errors[:linkedin]).not_to include("can't be blank")
+  end
+
+  it "returns a user's first_name as a string" do
+  end
+
+
   it "returns a user's last_name as a string"
   it "returns a user's email as a string"
   it "returns a user's username as a string"
@@ -25,4 +90,6 @@ RSpec.describe User, type: :model do
   it "returns a user's twitter url as a string"
   it "returns a user's github url as a string"
   it "returns a user's linkedin url as a string"
+end
+
 end
